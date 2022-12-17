c16efe327c2a931f287e98c8fd47cbb34e76329d
doroteea <doroteeasandor@yahoo.com>
2022-12-17 16:47:40 +0200

    started edit route

--- app/src/main/java/com/uid/smartmobilityapp/ui/home/HomeFragment.kt
+++ app/src/main/java/com/uid/smartmobilityapp/ui/home/HomeFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
@@ -28,10 +29,11 @@ class HomeFragment : Fragment() {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.homeTitleTextView
+        val travelNowButton: Button = binding.travelNowButtonId
         viewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+//            textView.text = it
         }
+
         return root
     }
 
