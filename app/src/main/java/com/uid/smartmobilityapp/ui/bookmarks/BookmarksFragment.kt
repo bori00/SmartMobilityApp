217dbece00a35d27ffdcb4f5e101d761a65b0703
bori00 <fazakasbori@gmail.com>
2022-12-18 15:37:52 +0200

    invoke action to go to add bookmark on button click

--- app/src/main/java/com/uid/smartmobilityapp/ui/bookmarks/BookmarksFragment.kt
+++ app/src/main/java/com/uid/smartmobilityapp/ui/bookmarks/BookmarksFragment.kt
@@ -5,14 +5,27 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.uid.smartmobilityapp.MainActivity
+import com.uid.smartmobilityapp.R
 import com.uid.smartmobilityapp.databinding.FragmentBookmarksBinding
+import com.uid.smartmobilityapp.ui.bookmarks.adapters.MyBookmarksRecyclerViewAdapter
+
 
 class BookmarksFragment : Fragment() {
 
     private var _binding: FragmentBookmarksBinding ? = null
+    lateinit private var _viewModel: BookmarksViewModel;
+    lateinit private var _root : View;
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -24,21 +37,44 @@ class BookmarksFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         Log.d("MainActivity", "Open Bookmarks Fragment")
-        val viewModel =
+        _viewModel =
             ViewModelProvider(this).get(BookmarksViewModel::class.java)
 
         _binding = FragmentBookmarksBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+        _root = binding.root
 
-        val textView: TextView = binding.bookmarkingFragmentTitleTextView
-        viewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-        return root
+        setupBookmarksRecyclerView()
+        setupAddNewBookmarkButton()
+
+        return _root
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun setupBookmarksRecyclerView() {
+        val bookmarksRecyclerView: RecyclerView = binding.myBookmarksRecyclerView
+        val layoutManager = LinearLayoutManager ( MainActivity.context, LinearLayoutManager.VERTICAL, false)
+        val adapter = MyBookmarksRecyclerViewAdapter(MainActivity.context, _viewModel.bookmarks)
+        bookmarksRecyclerView.layoutManager = layoutManager
+        bookmarksRecyclerView.adapter = adapter
+        addMessageDividers(bookmarksRecyclerView, layoutManager)
+    }
+
+    private fun setupAddNewBookmarkButton() {
+        val addBookmarkFAB: FloatingActionButton = binding.addNewBookmarkFAB
+        addBookmarkFAB.setOnClickListener { view ->
+            view.findNavController().navigate(R.id.action_nav_bookmarks_to_nav_add_bookmark)
+        }
+    }
+
+    private fun addMessageDividers(recyclerView: RecyclerView, linearLayoutManager : LinearLayoutManager) {
+        val dividerItemDecoration = DividerItemDecoration(
+            recyclerView.context,
+            linearLayoutManager.orientation
+        )
+        recyclerView.addItemDecoration(dividerItemDecoration)
     }
 }
--- app/src/main/java/com/uid/smartmobilityapp/ui/bookmarks/BookmarksViewModel.kt
+++ app/src/main/java/com/uid/smartmobilityapp/ui/bookmarks/BookmarksViewModel.kt
@@ -3,11 +3,9 @@ package com.uid.smartmobilityapp.ui.bookmarks
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.uid.smartmobilityapp.ui.bookmarks.model.MyBookmarks
 
 class BookmarksViewModel : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is the 'Bookmarks' Fragment"
-    }
-    val text: LiveData<String> = _text
+    val bookmarks = MyBookmarks.bookmarks
 }
