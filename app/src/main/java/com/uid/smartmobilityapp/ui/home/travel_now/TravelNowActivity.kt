9dc075d2e245d7c3c8f12961becc4e11f0dc32c9
doroteea <doroteeasandor@yahoo.com>
2022-12-19 14:28:03 +0200

    search in layout

--- app/src/main/java/com/uid/smartmobilityapp/ui/home/travel_now/TravelNowActivity.kt
+++ app/src/main/java/com/uid/smartmobilityapp/ui/home/travel_now/TravelNowActivity.kt
@@ -30,9 +30,9 @@ class TravelNowActivity : AppCompatActivity(), OnMapReadyCallback {
 
         val addressTF = findViewById<TextView>(R.id.addressTextId)
 
-        val mapFragment = supportFragmentManager
-            .findFragmentById(R.id.map) as SupportMapFragment
-        mapFragment.getMapAsync(this)
+//        val mapFragment = supportFragmentManager
+//            .findFragmentById(R.id.map) as SupportMapFragment
+//        mapFragment.getMapAsync(this)
 
 
     }
