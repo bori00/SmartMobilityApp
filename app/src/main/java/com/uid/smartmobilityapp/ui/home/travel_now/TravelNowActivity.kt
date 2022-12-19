3788fd635ff73063ddd662afde6888c58daad30a
doroteea <doroteeasandor@yahoo.com>
2022-12-19 12:39:46 +0200

    trying search places

--- app/src/main/java/com/uid/smartmobilityapp/ui/home/travel_now/TravelNowActivity.kt
+++ app/src/main/java/com/uid/smartmobilityapp/ui/home/travel_now/TravelNowActivity.kt
@@ -1,23 +1,66 @@
 package com.uid.smartmobilityapp.ui.home.travel_now
 
+import android.app.Activity
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
+import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.Nullable
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.libraries.places.api.Places
+import com.google.android.libraries.places.api.model.Place
+import com.google.android.libraries.places.api.net.PlacesClient
+import com.google.android.libraries.places.widget.Autocomplete
+import com.google.android.libraries.places.widget.model.AutocompleteActivityMode
 import com.uid.smartmobilityapp.R
 import com.uid.smartmobilityapp.VehicleListActivity
+import java.util.*
 
 class TravelNowActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_travel_now)
 
         val button = findViewById<Button>(R.id.searchRoutesButtonId)
         button.setOnClickListener {
-            val intent = Intent ( this, VehicleListActivity::class.java )
-            startActivity ( intent )
+            val intent = Intent(this, VehicleListActivity::class.java)
+            startActivity(intent)
         }
+
+        val addressTF = findViewById<TextView>(R.id.addressTextId)
+
+        //initialize places
+        Places.initialize(applicationContext, "AIzaSyByB3ZxKmq4KjtdnFDXSoD8axyXmxpEL6I")
+
+        val placesClient: PlacesClient
+
+
+        //set text view non focusable
+        addressTF.isFocusable = false
+//        addressTF.setOnClickListener {
+//            val fieldList: List<Place.Field> =
+//                Arrays.asList(Place.Field.ADDRESS, Place.Field.LAT_LNG, Place.Field.NAME)
+//            val intent: Intent =
+//                Autocomplete.IntentBuilder(AutocompleteActivityMode.OVERLAY, fieldList).build(this)
+//
+//            startActivityIntent.launch(intent)
+//
+////            startActivityForResult(intent,100)
+//
+//
+//        }
     }
+
+//    private val startActivityIntent =
+//        registerForActivityResult(
+//            ActivityResultContracts.StartActivityForResult()
+//        ) {
+//            if (it.resultCode == Activity.RESULT_OK) {
+//                Place place = Autocomplete . getPlaceFromIntent ()
+//
+//            }
+//        }
 }
 
