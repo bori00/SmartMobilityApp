e5cf9639c214f2d402cbfd2864572a43109e2e4e
bori00 <fazakasbori@gmail.com>
2022-12-19 12:50:47 +0200

    implement address search and display selected address on map

--- app/src/main/java/com/uid/smartmobilityapp/ui/bookmarks/AddBookmarkFragment.kt
+++ app/src/main/java/com/uid/smartmobilityapp/ui/bookmarks/AddBookmarkFragment.kt
@@ -1,11 +1,15 @@
 package com.uid.smartmobilityapp.ui.bookmarks
 
-import android.R
+import android.location.Address
+import android.location.Geocoder
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.SearchView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.google.android.gms.maps.CameraUpdateFactory
@@ -13,8 +17,11 @@ import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.MapView
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
+import com.uid.smartmobilityapp.MainActivity
 import com.uid.smartmobilityapp.databinding.FragmentAddBookmarkBinding
+import java.io.IOException
 
 
 class AddBookmarkFragment : Fragment(), OnMapReadyCallback {
@@ -22,9 +29,12 @@ class AddBookmarkFragment : Fragment(), OnMapReadyCallback {
     private var _binding: FragmentAddBookmarkBinding ? = null
     lateinit private var _viewModel: AddBookmarkViewModel;
 
-    private var mMapView: MapView? = null
+    private var _mMapView: MapView? = null
     private val MAPVIEW_BUNDLE_KEY = "MapViewBundleKey"
-    private lateinit var mMap: GoogleMap
+    private var _mMap: GoogleMap? = null
+    private lateinit var _searchView: SearchView
+
+    private var _selectedAddressMarker : Marker? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -42,6 +52,22 @@ class AddBookmarkFragment : Fragment(), OnMapReadyCallback {
         _binding = FragmentAddBookmarkBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
+        setupMap(savedInstanceState)
+        setupViewModel()
+
+        return root
+    }
+
+    override fun onMapReady(map: GoogleMap) {
+        _mMap = map
+    }
+
+    override fun onResume() {
+        _mMapView?.onResume()
+        super.onResume()
+    }
+
+    private fun setupMap(savedInstanceState: Bundle?) {
         // *** IMPORTANT ***
         // MapView requires that the Bundle you pass contain _ONLY_ MapView SDK
         // objects or sub-Bundles.
@@ -49,24 +75,69 @@ class AddBookmarkFragment : Fragment(), OnMapReadyCallback {
         if (savedInstanceState != null) {
             mapViewBundle = savedInstanceState.getBundle(MAPVIEW_BUNDLE_KEY)
         }
-        mMapView = binding.bookmarkMapView
-        mMapView!!.onCreate(mapViewBundle)
-        mMapView!!.getMapAsync(this)
+        _mMapView = binding.bookmarkMapView
+        _mMapView!!.onCreate(mapViewBundle)
 
-        return root
+        setupMapSearch()
+
+        _mMapView!!.getMapAsync(this)
     }
 
-    override fun onMapReady(map: GoogleMap) {
-        mMap = map
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    private fun setupMapSearch() {
+        // inspired by https://www.geeksforgeeks.org/how-to-add-searchview-in-google-maps-in-android/
+        _searchView = binding.bookmarkAddressSearchView
+        _searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String): Boolean {
+                val location = _searchView.query.toString()
+                var addressList: List<Address>? = null
+                if (location.isNotEmpty()) {
+                    val geocoder = Geocoder(MainActivity.context)
+                    try {
+                        addressList = geocoder.getFromLocationName(location, 1)
+                        if (!addressList.isEmpty()) {
+                            _viewModel.selectedAddress.value = addressList[0]
+                        } else {
+                            Toast.makeText(MainActivity.context, "This location couldn't be found. Please make your query more specific", Toast.LENGTH_SHORT).show()
+                        }
+                    } catch (e: IOException) {
+                        Toast.makeText(MainActivity.context, "This location couldn't be found. Please make your query more specific", Toast.LENGTH_SHORT).show()
+                        e.printStackTrace()
+                    }
+                } else {
+                    Toast.makeText(MainActivity.context, "Please select an address", Toast.LENGTH_SHORT).show()
+                }
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String): Boolean {
+                return false
+            }
+        })
     }
 
-    override fun onResume() {
-        mMapView?.onResume()
-        super.onResume()
+    private fun setupViewModel() {
+        // Name selection
+        val nameEditText: EditText = binding.bookmarkNameEditText
+        _viewModel.selectedName.observe(viewLifecycleOwner) {
+            nameEditText.setText(it)
+        }
+        // Address selection
+        _viewModel.selectedAddress.observe(viewLifecycleOwner) {
+            if (it != null && _mMap != null) {
+                val latLng = LatLng(it.latitude, it.longitude)
+                if (_selectedAddressMarker == null) {
+                    _selectedAddressMarker = _mMap!!.addMarker(
+                        MarkerOptions()
+                            .position(latLng))
+                } else {
+                    _selectedAddressMarker?.position = latLng
+                }
+                _selectedAddressMarker?.hideInfoWindow()
+                _selectedAddressMarker?.title = it.featureName
+                _selectedAddressMarker?.snippet = it.getAddressLine(0)
+                _selectedAddressMarker?.showInfoWindow()
+                _mMap!!.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15f))
+            }
+        }
     }
-
 }
--- app/src/main/java/com/uid/smartmobilityapp/ui/bookmarks/AddBookmarkViewModel.kt
+++ app/src/main/java/com/uid/smartmobilityapp/ui/bookmarks/AddBookmarkViewModel.kt
@@ -1,10 +1,16 @@
 package com.uid.smartmobilityapp.ui.bookmarks
 
+import android.location.Address
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.uid.smartmobilityapp.ui.bookmarks.model.MyBookmarks
 
 class AddBookmarkViewModel : ViewModel() {
-
+    val selectedAddress: MutableLiveData<Address> = MutableLiveData<Address>().apply {
+        value = null
+    }
+    val selectedName: LiveData<String> = MutableLiveData<String>().apply {
+        value = ""
+    }
 }
