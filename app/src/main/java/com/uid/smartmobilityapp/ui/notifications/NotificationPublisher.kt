85da1da05cbe5e50548c0a4fdaccaee9e5ca5138
bori00 <fazakasbori@gmail.com>
2023-01-05 01:41:51 +0200

    open up the relevant fragment when the notification is clicked

--- app/src/main/java/com/uid/smartmobilityapp/ui/notifications/NotificationPublisher.kt
+++ app/src/main/java/com/uid/smartmobilityapp/ui/notifications/NotificationPublisher.kt
@@ -2,14 +2,18 @@ package com.uid.smartmobilityapp.ui.notifications
 
 import android.app.Notification
 import android.app.NotificationManager
+import android.app.PendingIntent
+import android.app.TaskStackBuilder
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.util.Log
 import androidx.core.app.NotificationCompat
+import androidx.navigation.NavDeepLinkBuilder
 import com.uid.smartmobilityapp.MainActivity
 import com.uid.smartmobilityapp.R
 import com.uid.smartmobilityapp.ui.notifications.model.IntentNotification
+import com.uid.smartmobilityapp.ui.notifications.model.NotificationChannelTypes
 
 
 class NotificationPublisher : BroadcastReceiver() {
@@ -42,6 +46,21 @@ class NotificationPublisher : BroadcastReceiver() {
         builder.setContentText(content)
         builder.setSmallIcon(R.drawable.ic_baseline_schedule_24)
         builder.setChannelId(channelId)
+
+        val destination : Int;
+        if (channelId == NotificationChannelTypes.REGULAR_INTENT_NOTIFICATIONS_CHANNEL.data.channelId) {
+            destination = R.id.nav_regular_intents
+        } else {
+            destination = R.id.nav_flexible_intents
+        }
+        // Create an Intent for the activity you want to start
+        val resultIntent = NavDeepLinkBuilder(context)
+            .setComponentName(MainActivity::class.java)
+            .setGraph(R.navigation.mobile_navigation)
+            .setDestination(destination)
+            .createPendingIntent()
+
+        builder.setContentIntent(resultIntent)
         return builder.build()
     }
 }
--- app/src/main/java/com/uid/smartmobilityapp/ui/notifications/model/IntentNotificationsQueue.kt
+++ app/src/main/java/com/uid/smartmobilityapp/ui/notifications/model/IntentNotificationsQueue.kt
@@ -1,10 +1,12 @@
 package com.uid.smartmobilityapp.ui.notifications.model
 
+import com.uid.smartmobilityapp.ui.my_regular_intents.model.MyRegularIntents
 import java.time.LocalDateTime
 
 object IntentNotificationsQueue {
     val notifications: ArrayList<IntentNotification> = arrayListOf(
+        // TODO: synchronise data with the view intent task
         IntentNotification(1, "Get Groceries", "You should depart at 17:30 to have the most ecological ride. Expected arrival time: 17:45", NotificationChannelTypes.FLEXIBLE_INTENT_NOTIFICATIONS_CHANNEL, LocalDateTime.now().plusSeconds(20)),
-        IntentNotification(2, "Go to DBD class", "You should depart at 16:25 to arrive on time", NotificationChannelTypes.REGULAR_INTENT_NOTIFICATIONS_CHANNEL, LocalDateTime.now().plusMinutes(3)),
+        IntentNotification(2, MyRegularIntents.regularIntents.get(0), "You should depart at 16:25 to arrive on time", NotificationChannelTypes.REGULAR_INTENT_NOTIFICATIONS_CHANNEL, LocalDateTime.now().plusMinutes(3)),
     )
 }
