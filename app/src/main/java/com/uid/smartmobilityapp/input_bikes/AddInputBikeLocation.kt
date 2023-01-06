317962d60c4579b2b9325414a4ddc74b875e7ab4
doroteea <doroteeasandor@yahoo.com>
2023-01-06 23:39:46 +0200

    added add bike fragment_add_input_bikes.xml

--- app/src/main/java/com/uid/smartmobilityapp/input_bikes/AddInputBikeLocation.kt
+++ app/src/main/java/com/uid/smartmobilityapp/input_bikes/AddInputBikeLocation.kt
@@ -1,4 +1,181 @@
 package com.uid.smartmobilityapp.input_bikes
 
-class AddInputBikeLocation {
+import android.location.Address
+import android.location.Geocoder
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.*
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.MapView
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.uid.smartmobilityapp.MainActivity
+import com.uid.smartmobilityapp.R
+import com.uid.smartmobilityapp.databinding.FragmentAddInputBikesBinding
+import com.uid.smartmobilityapp.databinding.FragmentAddLocationBinding
+import com.uid.smartmobilityapp.input_bikes.model.InputBike
+import com.uid.smartmobilityapp.ui.travel_now.LocationsViewModel
+import com.uid.smartmobilityapp.ui.travel_now.model.Location
+import com.uid.smartmobilityapp.ui.travel_now.model.MyLocations
+import java.io.IOException
+
+class AddInputBikeLocation : Fragment(), OnMapReadyCallback {
+
+    private var _binding: FragmentAddInputBikesBinding? = null
+    lateinit private var _viewModel: InputBikeLocationViewModel;
+
+    private var _mMapView: MapView? = null
+    private val MAPVIEW_BUNDLE_KEY = "MapViewBundleKey"
+    private var _mMap: GoogleMap? = null
+
+    private lateinit var _searchView: SearchView
+
+    private var _selectedAddressMarker: Marker? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        Log.d("MainActivity", "Open Add Location Fragment")
+        _viewModel =
+            ViewModelProvider(this).get(InputBikeLocationViewModel::class.java)
+
+        _binding = FragmentAddInputBikesBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val nrBikes: TextView = binding.noBikesTF
+
+        val okButton: FloatingActionButton = binding.floatingActionButton2
+        okButton.setOnClickListener {
+            if(nrBikes.text.toString() == ""){
+//                makeTpa
+            }
+
+
+
+                binding.root.findNavController().navigate(R.id.action_travel_now_to_vehicle_list)
+
+        }
+
+        setupMap(savedInstanceState)
+        setupViewModel()
+        return root
+    }
+
+    override fun onMapReady(map: GoogleMap) {
+        _mMap = map
+    }
+
+    override fun onResume() {
+        _mMapView?.onResume()
+        super.onResume()
+    }
+
+    private fun setupMap(savedInstanceState: Bundle?) {
+        // *** IMPORTANT ***
+        // MapView requires that the Bundle you pass contain _ONLY_ MapView SDK
+        //objects or sub-Bundles.
+        var mapViewBundle: Bundle? = null
+        if (savedInstanceState != null) {
+            mapViewBundle = savedInstanceState.getBundle(MAPVIEW_BUNDLE_KEY)
+        }
+        _mMapView = binding.mapView
+        _mMapView!!.onCreate(mapViewBundle)
+
+        setupMapSearch()
+
+        _mMapView!!.getMapAsync(this)
+    }
+
+    private fun setupMapSearch() {
+        // inspired by https://www.geeksforgeeks.org/how-to-add-searchview-in-google-maps-in-android/
+        _searchView = binding.getLocationSearchView
+
+        _searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String): Boolean {
+                val location = _searchView.query.toString()
+                var addressList: List<Address>? = null
+                if (location.isNotEmpty()) {
+                    val geocoder = Geocoder(MainActivity.context)
+                    try {
+                        addressList = geocoder.getFromLocationName(location, 1)
+                        if (!addressList.isEmpty()) {
+                            _viewModel.selectedAddress.value = addressList[0]
+                            _viewModel.query.value = query
+
+                            _viewModel.input_bikes.value?.add(
+                                InputBike(
+                                    query,
+                                    "",
+                                    _viewModel.selectedAddress.value!!
+                                )
+                            )
+                        } else {
+                            Toast.makeText(
+                                MainActivity.context,
+                                "This location couldn't be found. Please make your query more specific",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    } catch (e: IOException) {
+                        Toast.makeText(
+                            MainActivity.context,
+                            "This location couldn't be found. Please make your query more specific",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        e.printStackTrace()
+                    }
+                } else {
+                    Toast.makeText(
+                        MainActivity.context,
+                        "Please select an address",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String): Boolean {
+                return false
+            }
+        })
+    }
+
+    private fun setupViewModel() {
+        // Address selection
+        _viewModel.selectedAddress.observe(viewLifecycleOwner) {
+            if (it != null && _mMap != null) {
+                val latLng = LatLng(it.latitude, it.longitude)
+                if (_selectedAddressMarker == null) {
+                    _selectedAddressMarker = _mMap!!.addMarker(
+                        MarkerOptions()
+                            .position(latLng)
+                    )
+                } else {
+                    _selectedAddressMarker?.position = latLng
+                }
+                _selectedAddressMarker?.hideInfoWindow()
+                _selectedAddressMarker?.title = it.featureName
+                _selectedAddressMarker?.snippet = it.getAddressLine(0)
+                _selectedAddressMarker?.showInfoWindow()
+                _mMap!!.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15f))
+            }
+        }
+    }
+
 }
--- app/src/main/java/com/uid/smartmobilityapp/input_bikes/InputBikeLocationViewModel.kt
+++ app/src/main/java/com/uid/smartmobilityapp/input_bikes/InputBikeLocationViewModel.kt
@@ -1,10 +1,13 @@
 package com.uid.smartmobilityapp.input_bikes
 
+import android.location.Address
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.uid.smartmobilityapp.input_bikes.model.InputBike
 import com.uid.smartmobilityapp.input_bikes.model.MyInputBikes
+import com.uid.smartmobilityapp.models.AddressWithName
+import com.uid.smartmobilityapp.services.DeviceLocationProviderService
 
 class InputBikeLocationViewModel : ViewModel() {
 
@@ -12,5 +15,13 @@ class InputBikeLocationViewModel : ViewModel() {
         MutableLiveData<ArrayList<InputBike>>().apply {
             value = MyInputBikes.input_bikes
         }
+
+    val selectedAddress: MutableLiveData<Address> = MutableLiveData<Address>().apply {
+        value = null
+    }
+
+    val query: MutableLiveData<String> = MutableLiveData<String>().apply {
+        value = null
+    }
 
 }
