b77066a05fa60b64c022e610ed78d03530fd06b2
iulrus <iulrus@bitdefender.com>
2023-01-16 14:51:38 +0200

    Removed feedback functionality

--- app/src/main/java/com/uid/smartmobilityapp/ui/travel_now/ViewFinalRouteFragment.kt
+++ app/src/main/java/com/uid/smartmobilityapp/ui/travel_now/ViewFinalRouteFragment.kt
@@ -46,7 +46,6 @@ class ViewFinalRouteFragment : Fragment() {
         _root = binding.root
         setupSearchRoutesButton()
         setupHomeButton()
-        setupFeedbackButton()
         setupLocationSummary()
         setupRouteImage()
         return _root
@@ -56,13 +55,6 @@ class ViewFinalRouteFragment : Fragment() {
         val homeButton: Button = binding.homeButtonID
         homeButton.setOnClickListener { view ->
             binding.root.findNavController().navigate(R.id.action_view_route_to_home)
-        }
-    }
-
-    private fun setupFeedbackButton() {
-        val homeButton: Button = binding.feedbackButtonID
-        homeButton.setOnClickListener { view ->
-            binding.root.findNavController().navigate(R.id.action_final_route_to_feedback)
         }
     }
 
